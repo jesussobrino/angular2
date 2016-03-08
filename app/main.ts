5e64875539e61c12ce58a23157305174346ae12e
Jesús Sobrino <jesusyyo@gmail.com>
2016-03-08 18:47:33 +0100

    feat(*): add polymer library and component example with angular2

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
+import {GoogleMapsAppComponent} from './google-maps-app.component'
 
-bootstrap(AppComponent);
+bootstrap(GoogleMapsAppComponent);
